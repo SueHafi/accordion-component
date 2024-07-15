71e761631b8df9a84607b7cf02a0d01116cc08a6
sue rose <suehaizoglu@gmail.com>
2024-07-15 13:02:37 +0100

    made accessibile

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -8,8 +8,8 @@ type AccordionProps = {
 
 export default function Accordion({ num, title, children }: AccordionProps) {
   const [isExpanded, setIsExpanded] = useState(false);
-  
-  function handleAccordionClick(e: MouseEvent<HTMLButtonElement>) {
+
+  function handleAccordionClick(): void {
     setIsExpanded(!isExpanded);
   }
 
@@ -17,21 +17,34 @@ export default function Accordion({ num, title, children }: AccordionProps) {
     <div className="accordion-container">
       <h2 className="accordion-header">
         <button
+          id="accordion-controls"
           className="accordion-button"
           onClick={handleAccordionClick}
           type="button"
           aria-expanded={isExpanded}
+          aria-controls="accordion-panel"
         >
           <span className="accordion-num">{num}</span>
           <span className="accordion-title">{title}</span>
           <img
             className="accordion-symbol"
             alt="caret down"
-            src="/caret-down.png"
+            src={`${isExpanded ? "/caret-up.png" : "/caret-down.png"}`}
           />
         </button>
       </h2>
-      <section className={`${isExpanded? 'accordion-text-section' : 'accordion-text-section hidden'}`}>{children}</section>
+      <section
+        id="accordion-panel"
+        className={`${
+          isExpanded
+            ? "accordion-text-section"
+            : "accordion-text-section hidden"
+        }`}
+        role="region"
+        aria-labelledby="accordion-controls"
+      >
+        {children}
+      </section>
     </div>
   );
 }
